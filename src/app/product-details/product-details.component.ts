3ac1ec208bebbb4d195875fc890b38f74941654d
Kameron Hartman <47125766+kamsdogs@users.noreply.github.com>
2021-05-04 21:39:34 -0400

    Define & Use Cart Service
    Define & Use Cart Service

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { products } from '../products';
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-product-details',
@@ -8,9 +9,12 @@ import { products } from '../products';
   styleUrls: ['./product-details.component.css']
 })
 export class ProductDetailsComponent implements OnInit {
-  product: any;
-  constructor(private route: ActivatedRoute,) { }
-
+  product;
+  constructor(private route: ActivatedRoute, private cartService: CartService) { }
+  addToCart(product){
+    this.cartService.addToCart(product);
+    window.alert('Product added to cart!')
+  }
   ngOnInit(): void {
     // First get the product id from the current route.
     const routeParams = this.route.snapshot.paramMap;
